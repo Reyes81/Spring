c1d488d527408dfcf7c91c316b34eeba1978cc9e
Alejandro <alejandro.reyes@uv.es>
2022-05-02 14:57:29 +0200

    Empieza la Fiesta!

---
+++ ProyectoFinal_Consumidores/src/test/java/com/proyecto_final_consumidores/ProyectoFinalConsumidoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto_final_consumidores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalConsumidoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
