61442ed9e895c70831ccfc50cfb3277253bc2ce8
Alejandro <alejandro.reyes@uv.es>
2022-05-11 15:21:14 +0200

    Update ValidadoresController.java

--- ApiValidadores/src/main/java/com/api_validadores/controller/ValidadoresController.java
+++ ApiValidadores/src/main/java/com/api_validadores/controller/ValidadoresController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.apil_validadores.domain.Informador;
+import com.api_validadores.domain.Informador;
 
 @RestController
 @RequestMapping(value="/api/validador")
