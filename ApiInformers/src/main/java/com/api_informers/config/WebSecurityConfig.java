585c54f1896ad8f50ecef392094511c295e03e3a
Alejandro <alejandro.reyes@uv.es>
2022-05-15 12:38:53 +0200

    Security tag02

--- ApiInformers/src/main/java/com/api_informers/config/WebSecurityConfig.java
+++ ApiInformers/src/main/java/com/api_informers/config/WebSecurityConfig.java
@@ -1,13 +1,22 @@
 package com.api_informers.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,5 +28,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
             .formLogin();
     }
-
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+        	.inMemoryAuthentication()
+        	.withUser("informer").password(passwordEncoder().encode("1234")).roles("INFORMER")
+        	.and()
+        	.withUser("validator").password(passwordEncoder().encode("1234")).roles("VALIDATOR");
+    }
 }
